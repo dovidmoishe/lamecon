8355e29f713252172a2e924a8d412131fba25f27
David Baiye <ozerlihashem@gmail.com>
2024-02-03 12:20:57 +0100

    hero fix

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -20,11 +20,11 @@ const Hero: React.FC = () => {
     // };
     return (
         <div className="h-screen w-full relative">
-            <div className="absolute inset-0 flex pt-15 flex-col justify-center items-center text-black bg-opacity-10 h-screen">
+            <div className="absolute inset-0 flex pt-15 flex-col justify-center items-center text-black bg-opacity-10 h-screen px-2">
                 <Text>
                     <h1 className="text-6xl font-bold text-center">Nuturing Africa future</h1>
                 </Text>
-                <Text className="text-lg mt-5 text-black"> Together we make the world a better place
+                <Text className="text-lg mt-5 text-black text-center"> Together we make the world a better place
                 </Text>
                 <div className=" flex flex-row justify-center items-center text-white bg-opacity-50 pt-5 ">
                     <Button className="bg-blue-500  text-white font-bold rounded-full">
