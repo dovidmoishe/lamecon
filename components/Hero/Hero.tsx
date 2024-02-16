897cf22f332d8903af361ec9b02580df0fe511d8
David Baiye <ozerlihashem@gmail.com>
2024-02-16 13:30:59 +0100

    work ooooo

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -8,18 +8,18 @@ const Hero: React.FC = () => {
 
     return (
         <div className={`h-screen w-full relative`}>
-            {/* <Image
-                className="w-full h-full bg-cover object-cover z-40 hidden md:block"
+            <Image
+                className="w-full h-full bg-cover object-cover  hidden md:block"
                 src={`https://i.imgur.com/upNsB54.jpeg`}
                 alt="hero_img"
                 layout="fill"
             />
             <Image
-                className="w-full h-full bg-cover object-cover z-40 md:hidden  "
+                className="w-full h-full bg-cover object-cover  md:hidden  "
                 src={`https://i.imgur.com/Nkg5zhj.jpeg`}
                 alt="hero_img"
                 layout="fill"
-            /> */}
+            />/
             <div className="absolute inset-0 flex flex-col md:items-start justify-center items-center px-8 ">
                 <h1 className="text-5xl font-bold text-white text-center md:text-left md:text-black">Nuturing Africa&apos;s Future</h1>
                 <p className="mt-3 text-lg text-center md:text-left text-white md:text-black">Together we make the world a better place</p>
